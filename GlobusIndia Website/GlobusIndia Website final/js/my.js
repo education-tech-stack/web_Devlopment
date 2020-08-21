83c8d52051b41b57fb08e6f8e2802343d55fde91
Sameer Chauhan <sameer55chauhan@gmail.com>
2020-08-22 01:25:17 +0530

    Globus India website Completed

--- GlobusIndia Website/GlobusIndia Website final/js/my.js
+++ GlobusIndia Website/GlobusIndia Website final/js/my.js
@@ -26,3 +26,22 @@ function openNav() {
 function closeNav() {
   document.getElementById("myNav").style.height = "0%";
 }
+
+var modal = document.getElementsByClassName('w3-modal');
+var i=0;
+window.onclick = function(event) {
+  for(i=0; i<modal.length; i++){
+    if (event.target == modal[i]) {
+      modal[i].style.display = "none";
+    }
+  }
+}
+
+function openImg(imgName) {
+  var i, x;
+  x = document.getElementsByClassName("picture");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(imgName).style.display = "block";
+}
